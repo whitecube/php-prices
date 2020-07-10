6f5b88cfbf89814b76731bfa2fa88a5c33351076
Nuno Maduro <enunomaduro@gmail.com>
2020-07-10 15:22:49 +0200

    No need for `this` in tests

--- tests/Unit/ForwardsToMoneyTest.php
+++ tests/Unit/ForwardsToMoneyTest.php
@@ -8,9 +8,9 @@ use Whitecube\Price\Price;
 it('creates instance from Money API', function() {
     $instance = Price::EUR(500, 5);
 
-    $this->assertInstanceOf(Price::class, $instance);
-    $this->assertInstanceOf(Money::class, $instance->base());
-    $this->assertEquals(5, $instance->units());
+    assertInstanceOf(Price::class, $instance);
+    assertInstanceOf(Money::class, $instance->base());
+    assertEquals(5, $instance->units());
 });
 
 it('forwards modifications to base Money instance', function() {
@@ -19,5 +19,5 @@ it('forwards modifications to base Money instance', function() {
 
     $price->subtract(Money::EUR(100));
 
-    $this->assertTrue($price->equals(Money::EUR(400)));
+    assertTrue($price->equals(Money::EUR(400)));
 });
