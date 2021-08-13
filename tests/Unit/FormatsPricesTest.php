e6550e76cc8c5f60f475837eeb248571e43c7ac1
Toon Van den Bos <toon@whitecube.be>
2021-08-13 15:05:00 +0200

    Added formatter brief as tests

---
+++ tests/Unit/FormatsPricesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use Brick\Money\Money;
+use Whitecube\Price\Price;
+
+it('formats Price instances as inclusive localized strings using application locale', function() {
+
+});
+
+it('formats Brick\\Money instances as localized strings using application locale', function() {
+
+});
+
+it('formats Vat instances as localized strings using application locale', function() {
+
+});
+
+it('formats monetary values using provided locale', function() {
+
+});
+
+it('formats monetary values using a previously defined custom formatted closure', function() {
+
+});
+
+it('formats monetary values using the default formatter despite of the previously defined custom formatted closure', function() {
+
+});
+
+it('formats monetary values using one of the previously defined custom named formatted closures', function() {
+
+});
+
+it('formats monetary values using forwarded method parameters on a previously defined custom formatted closure', function() {
+
+});
